ec3b9ac36e12c4d2c1203ee635d7df26ec996a67
Brandon <bbills@terpmail.umd.edu>
2025-05-16 19:30:39 -0400

    vercel update

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const supabaseClient = require('@supabase/supabase-js');
 const bodyParser = require('body-parser');
-
+const dotenv = require('dotenv');
+dotenv.config(); // Load environment variables from .env file
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY);
 
 const app = express();
 const port = 3000;
@@ -10,8 +13,8 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public')); // Serve static files from the 'public' directory
 app.use(express.json()); // Parse JSON bodies
 
-const supabaseUrl = 'https://wcjlbaqtdgvyeyoshmii.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjamxiYXF0ZGd2eWV5b3NobWlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczNjI1MjEsImV4cCI6MjA2MjkzODUyMX0.KuGkioNPe1mw_JiZnygP41AmRLAAJcnoogqlV9Mi6Tw';
+const supabaseUrl = process.env.SUPABASE_URL; // Use environment variable for security
+const supabaseKey = process.env.SUPABASE_KEY; // Use environment variable for security
 const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
 
 app.get('/api/data', async (req, res) => {
